26633ecfa4df59a85da962ad5eb4d74649411cd9
WigiWigi1 <minlebaevr@gmail.com>
2024-09-10 22:02:58 +0200

    new commit with logic only

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+
+#Creating a project of syncing files into master and replica folder.
+
+#Get 2 args of input master folder and replica folder
+
+
+
+#If number of arguments != 2, we would exit and print that exactly 2 arguments needed
+
+#getting list of files in master folder
+
+#getting list of files in replica folder
+
+#if file not in replica folder -> copy from master, log action into console and log
+
+#if file exist in replica but not exist in master -> delete from replica, log action into console and log
+
+#if file exist in both folders -> check if file parameters are same, if not -> delete from replica and copy from master,
+# log action into console and log
+
+
